f1631e6608c6c785b0d8bcc820eef019d7ecfa6a
Mark Kalinovits <kalee.mark@gmail.com>
2016-06-19 06:59:33 +0200

    finalizing namespace code adding namespace to css classes

--- app/StatusNavigation/src/main/javascript/StatusNavigation.js
+++ app/StatusNavigation/src/main/javascript/StatusNavigation.js
@@ -12,17 +12,23 @@
  */
 (function (angular) {
     angular.module('StatusNavigation', [])
-        .directive('mnStatusNavigation', [function () {
+        .directive('mndStatusNavigation', [function () {
             var config;
+            var cssClasses = {
+                default: '',
+                active: 'mnd-element-active',
+                done: 'mnd-element-done',
+                disabled: 'mnd-element-disabled'
+            };
             var getClass = function (real_index) {
                 var index = real_index + 1,
-                    result = '';
+                    result = cssClasses.default;
                 if (index === config.active) {
-                    result = 'active';
+                    result = cssClasses.active;
                 } else if (index < config.active) {
-                    result = 'done';
+                    result = cssClasses.done;
                 } else if (config.disabled.indexOf(index) !== -1) {
-                    result = 'disabled';
+                    result = cssClasses.disabled;
                 }
                 return result;
             };
@@ -42,9 +48,9 @@
                 scope: {
                     config: '='
                 },
-                template: '<div class="status-navigation">' +
-                '<div class="navigation-icon-bar"><ul><li ng-repeat="element in config.elements" ng-class="getClass($index)"><div class="icon">{{element.icon}}</div></li></ul></div>' +
-                '<div class="navigation-text-bar"><ul><li ng-repeat="element in config.elements" ng-class="getClass($index)">{{element.text}}</li></ul></div>' +
+                template: '<div class="mnd-status-navigation">' +
+                '<div class="mnd-navigation-icon-bar"><ul><li ng-repeat="element in config.elements" ng-class="getClass($index)"><div class="mnd-number-icon">{{element.icon}}</div></li></ul></div>' +
+                '<div class="mnd-navigation-text-bar"><ul><li ng-repeat="element in config.elements" ng-class="getClass($index)">{{element.text}}</li></ul></div>' +
                 '</div>',
                 link: function (scope) {
                     config = scope.config;
